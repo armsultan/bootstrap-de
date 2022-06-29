dcf3fae086dbbb421087a09f8ab8f38b856b78ba
Armand <armsultan@gmail.com>
2022-06-28 19:18:12 -0600

    Update de-apt-mate.sh

--- desktop-environment/apt/mate-xrdp/de-apt-mate.sh
+++ desktop-environment/apt/mate-xrdp/de-apt-mate.sh
@@ -5,11 +5,6 @@ apt-get update
 apt-get install xrdp ssl-cert ubuntu-mate-desktop -y
 apt-get clean
 
-# UFW firewall
-# ufw allow 3389
-# ufw allow from 192.168.1.0/24 to any port 3389 
-# ufw reload 
-
 # Setup the necessary permissions
 # Allow xrdp user to access "/etc/ssl/private/ssl-cert-snakeoil.key”
 usermod -a -G ssl-cert xrdp
@@ -17,6 +12,14 @@ usermod -a -G ssl-cert xrdp
 # xRDP – Allow multiple sessions (local and remote) for the same user
 # Workaround – unsetting DBUS_SESSION_BUS_ADRESS approach
 wget https://raw.githubusercontent.com/armsultan/bootstrap-de/main/desktop-environment/apt/mate-xrdp/startwm.sh -O /etc/xrdp/startwm.sh
+
+systemctl enable --now xrdp
+
+# UFW firewall
+# ufw allow 3389
+# ufw allow from 192.168.1.0/24 to any port 3389 
+ufw allow from any to any port 3389 proto tcp
+ufw reload 
 
 echo " "
 echo "XRDP has Started"
